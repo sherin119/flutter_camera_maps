65f73a61df0207b0b818b6871d40987abb3168e5
sherin119 <sherin119@gmail.com>
2021-03-25 11:37:03 +0530

    Initial commit

--- lib/helpers/location_helper.dart
+++ lib/helpers/location_helper.dart
@@ -1,22 +1,17 @@
 import 'dart:convert';
 
 import 'package:http/http.dart';
-
-const apiKey =
-'pk.eyJ1Ijoic2hlcmluMTE5IiwiYSI6ImNraGMwNDE4cTA2Y3QzMm4xd3Q1d2piZHIifQ.5HXKoRYCTa5f1MGSiy-11w';
+import 'package:flutter_camera_maps/helpers/api_key.dart';
 
 class LocationHelper {
   static String generateLocationPreviewImage({double latitude, double longitude}){
     return 'https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/pin-l($longitude,$latitude)/$longitude,$latitude,14.25,0,0/600x300?access_token=$apiKey';
-
   }
 
   static Future<String> getPlaceAddress(double lat, double lng) async {
     final url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/$lng,$lat.json?access_token=$apiKey';
     final response = await get(url);
     return jsonDecode(response.body)['features'][0]['place_name'];
-
-
   }
   }
 
